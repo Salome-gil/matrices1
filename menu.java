396e9b41a64a06aabed9e3af74940dff57d7bbfc
Sala N 404 <salan404@salasfr.itm.edu.co>
2024-08-09 20:07:19 -0500

    punto 9

--- menu.java
+++ menu.java
@@ -4,6 +4,8 @@ public class menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);{
             int[] opciones ={1,2,3,4,5,6,7,8,9,10,11};
+            Punto9  p9 = new Punto9();
+
             int seleccion =-1;
             while (seleccion != 0) {
                 System.out.println("Menu principal");
@@ -11,11 +13,14 @@ public class menu {
                     // System.out.println(opciones[i]+ "Opcion"+opciones[i]);
                 }
 
-                System.out.println("Seleccione el ejercicio que desea ejercutar");
                 System.out.println("Digite ejercicios del 1 al 11");
-                System.out.println("Digite 0 para salir");
                 seleccion = sc.nextInt();
-                if (seleccion >= 1 && seleccion <=opciones.length) {
+                // System.out.println("Digite 0 para salir");
+                int dim = 0;
+                System.out.println("ingrese la dimencion de la matriz");
+                dim = sc.nextInt();
+               
+                // if (seleccion >= 1 && seleccion <=opciones.length) {
                     switch (seleccion) {
                         case 0:
                         System.out.println("Adios");
@@ -38,6 +43,8 @@ public class menu {
                         case 8:
                             break;
                         case 9:
+                        int result= p9.maximon(dim);
+                        System.out.println("el numero mayor en la matriz es :" + result);
                             break;
                         case 10:
                             break;
@@ -47,7 +54,7 @@ public class menu {
                         System.out.println("Opcion no valida");
                         
                     }
-                }
+                // }
             }  
 
 
