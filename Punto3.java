8b7e8553ffc125ae16dedf23145e4b650cd9227e
SrAlejo4 <alejandrogarci087@gmail.com>
2024-08-14 23:04:03 -0500

    Se agrega el punto 3

---
+++ Punto3.java
@@ -0,0 +1,41 @@
+public class Punto3 {
+    public void ejercicio3(){
+        int tam = 5;
+        int [][] matriz3 = llenarMatriz(tam);
+        int [] sumFilas = new int[tam];
+        int [] sumColumnas = new int [tam];
+        int sumaF, sumaC;
+
+        System.out.println("\n ----- EJERCICIO 3 ----- \n");
+
+        // Suma de filas y agregación a arreglo ...
+        for(int i = 0; i < tam; i++){
+            sumaF = 0;
+            for(int j = 0; j < tam; j++){
+                sumaF += matriz3[i][j];
+            }
+            sumFilas[i] = sumaF;
+        }
+
+        // Suma de columnas y agregación a arreglo ... 
+        for(int i = 0; i < tam; i++){
+            sumaC = 0;
+            for(int j = 0; j < tam; j++){
+                sumaC += matriz3[j][i];
+            }
+            sumColumnas[i] = sumaC;
+        }
+
+        // Se recorre arreglo de suma de filas y se imprime ...
+        System.out.println("\nLa suma de las filas es : ");
+        for(int i = 0; i < sumFilas.length; i++){
+            System.out.print(sumFilas[i] + " ");
+        }
+
+        // Se recorre arreglo de suma de columnas y se imprime ...
+        System.out.println("\nLa suma de las columnas es : ");
+        for(int i = 0; i < sumColumnas.length; i++){
+            System.out.print(sumColumnas[i] + " ");
+        }
+    }
+}
