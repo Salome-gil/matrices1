365c58defbd6d96baece261022e5a79882b4dc49
H4D3Sz <hadesdds1308@gmail.com>
2024-08-15 20:00:46 -0500

    Mostrar

---
+++ Mostrar.java
@@ -0,0 +1,11 @@
+public class Mostrar{
+    public void mostrar(int[][]matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                
+                System.out.println(matriz[i][j] + "\n");
+            }
+        }
+    
+    }
+}
